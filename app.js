91cc285d91e3ad210a8ee5c6c2b65aa6503efde5
Gabriel Fallas <gabrielfallas0803@gmail.com>
2022-12-20 21:34:30 -0600

    ADD desencriptador

--- app.js
+++ app.js
@@ -23,5 +23,32 @@ const encriptarTexto = (texto) => {
 	palabras.forEach((palabra, indice) => {
 		palabras[indice] = encriptarPalabra(palabra);
 	});
+	//Se toman las palabras del arreglo y se unen en un solo string.
+	return palabras.join(" ");
+};
+/**
+ *
+ * @param {String} palabra
+ */
+const desencriptarPalabra = (palabra) => {
+	palabra = palabra.replace("ai", "a");
+	palabra = palabra.replace("enter", "e");
+	palabra = palabra.replace("imes", "i");
+	palabra = palabra.replace("ober", "o");
+	palabra = palabra.replace("ufat", "u");
+	return palabra;
+};
+/**
+ *
+ * @param {String} texto
+ */
+const desencriptarTexto = (texto) => {
+	//Se quitan los espacios extra del texto.
+	const palabras = texto.replace(/\s+/g, " ").split(" ");
+	//Se recorre el array y se encripta cada palabra.
+	palabras.forEach((palabra, indice) => {
+		palabras[indice] = desencriptarPalabra(palabra);
+	});
+	//Se toman las palabras del arreglo y se unen en un solo string.
 	return palabras.join(" ");
 };
